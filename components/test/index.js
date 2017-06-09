5c51aa7eca225a84acff0ab10fb2ebeffcace759
Brian Gouws <brian.gouws@bauermedia.co.uk>
2017-06-09 16:24:52 +0100

    Update: added helper and partial support (refs #PR-85)

--- components/test/index.js
+++ components/test/index.js
@@ -1,14 +1,16 @@
 'use strict'
-
+/* global $ */
 module.exports = function () {
-  var btn = document.getElementById('bauer-web-component-test-btn')
-  if (typeof btn === 'object' && btn !== null) {
-    btn.addEventListener('click', function () {
-      if (btn.style.backgroundColor === 'red') {
-        btn.style.backgroundColor = 'white  '
-      } else {
-        btn.style.backgroundColor = 'red'
-      }
-    })
-  }
+  $(function () {
+    var btn = document.getElementById('bauer-web-component-test-btn')
+    if (typeof btn === 'object' && btn !== null) {
+      btn.addEventListener('click', function () {
+        if (btn.style.backgroundColor === 'red') {
+          btn.style.backgroundColor = 'white  '
+        } else {
+          btn.style.backgroundColor = 'red'
+        }
+      })
+    }
+  })
 }
---
+++ docs/dist/partials/partials2.js
@@ -0,0 +1,1 @@
+(function(dust){dust.register("partials2",body_0);function body_0(chk,ctx){return chk.w("partial2.dust");}body_0.__dustBody=!0;return body_0}(dust));
