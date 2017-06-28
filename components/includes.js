bb70d0fe49f8c3ed68fc3ed3ea63915fbb787174
Ross Pickerill <ross.pickerill@bauerxcel.co.uk>
2017-06-28 16:59:12 +0100

    Shared libs update for UI

--- components/includes.js
+++ components/includes.js
@@ -2,6 +2,8 @@
 
 // include shared libs
 require('./shared/js/main.js')
+window.jQuery = require('jQuery')
+require('./shared/sass/bootstrap-sass-3.3.7/assets/javascripts/bootstrap.min.js')
 
 // add desired required compoenents to ensure files are included in the build script
 require('./test')(window)
