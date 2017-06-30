1fdbbe881a0f9f96130552e40610a31778f512c5
Ross Pickerill <ross.pickerill@bauerxcel.co.uk>
2017-06-30 16:55:49 +0100

    Update: Update dust to pug (refs #TEMP-7)

---
+++ docs/dist/partials/syndicate-time.js
@@ -0,0 +1,78 @@
+function pug_attr(t,e,n,f){return e!==!1&&null!=e&&(e||"class"!==t&&"style"!==t)?e===!0?" "+(f?t:t+'="'+t+'"'):("function"==typeof e.toJSON&&(e=e.toJSON()),"string"==typeof e||(e=JSON.stringify(e),n||e.indexOf('"')===-1)?(n&&(e=pug_escape(e))," "+t+'="'+e+'"'):" "+t+"='"+e.replace(/'/g,"&#39;")+"'"):""}
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function partials_syndicate-time(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (data, settings, syndicateCategoryLink, window) {;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fmixins\u002FrelativeTimeUTC.pug";
+pug_mixins["relativeTimeUTC"] = pug_interp = function(data){
+var block = (this && this.block), attributes = (this && this.attributes) || {};
+;pug_debug_line = 2;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fmixins\u002FrelativeTimeUTC.pug";
+var relativeTimeUTC = window.bauerSharedLibs.pug.mixins.relativeTimeUTC(data)
+;pug_debug_line = 3;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fmixins\u002FrelativeTimeUTC.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = relativeTimeUTC) ? "" : pug_interp));
+};
+;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fmixins\u002FsyndicateCategory.pug";
+
+
+
+
+
+
+
+;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fmixins\u002FsyndicateCategoryLink.pug";
+
+
+
+
+
+
+
+;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+var syndicateCategoryLinkData = {
+    article: data.article,
+    channel: settings.channel,
+    displaySyndicate: data.displaySyndicate
+}
+
+var syndicateCategoryData = {
+    article: data.article,
+    channel: settings.channel,
+    displaySyndicate: data.displaySyndicate,
+    displayField: 'name'
+}
+
+;pug_debug_line = 19;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+if (data.primarySyndicatePosition != '') {
+;pug_debug_line = 20;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_html = pug_html + "\u003Cspan class=\"hidden-xs modified-container\"\u003E";
+;pug_debug_line = 21;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_html = pug_html + "\u003Ca" + (" class=\"brand-color\""+pug_attr("href", +syndicateCategoryLink(syndicateCategoryLinkData), true, false)) + "\u003E";
+;pug_debug_line = 21;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_html = pug_html + "+syndicateCategory(syndicateCategoryData)\u003C\u002Fa\u003E";
+;pug_debug_line = 22;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+if (data.lastModifiedAt) {
+;pug_debug_line = 23;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_mixins["relativeTimeUTC"](data.lastModifiedAt);
+}
+else {
+;pug_debug_line = 25;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_mixins["relativeTimeUTC"](data.publicationDate);
+}
+pug_html = pug_html + "\u003C\u002Fspan\u003E";
+}
+else {
+;pug_debug_line = 27;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_html = pug_html + "\u003Cspan class=\"hidden-xs modified-container\"\u003E";
+;pug_debug_line = 28;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_html = pug_html + "\u003Ca" + (" class=\"brand-color\""+pug_attr("href", '/'+data.syndicateUrls+'/', true, false)) + "\u003E";
+;pug_debug_line = 28;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_html = pug_html + "data.categories[0].name\u003C\u002Fa\u003E";
+;pug_debug_line = 29;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+if (data.lastModifiedAt) {
+;pug_debug_line = 30;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_mixins["relativeTimeUTC"](data.lastModifiedAt);
+}
+else {
+;pug_debug_line = 32;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Fsyndicate-time.pug";
+pug_mixins["relativeTimeUTC"](data.publicationDate);
+}
+pug_html = pug_html + "\u003C\u002Fspan\u003E";
+}}.call(this,"data" in locals_for_with?locals_for_with.data:typeof data!=="undefined"?data:undefined,"settings" in locals_for_with?locals_for_with.settings:typeof settings!=="undefined"?settings:undefined,"syndicateCategoryLink" in locals_for_with?locals_for_with.syndicateCategoryLink:typeof syndicateCategoryLink!=="undefined"?syndicateCategoryLink:undefined,"window" in locals_for_with?locals_for_with.window:typeof window!=="undefined"?window:undefined));} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line);};return pug_html;}
---
+++ docs/examples/large-content-card.js
@@ -0,0 +1,80 @@
+'use strict'
+
+window.bauerSharedLibs.pug.render('large-content-card/large-content-card.pug', {'settings': {
+  'protocol': 'http://',
+  'imageQuality': '80',
+  'imageDomain': 'mediatest.planetradio.co.uk',
+  'channel': 'magic',
+  'stationHeaderLogoSVG': 'https://assets.planetradio.co.uk/img/ConfigWebHeaderLogoSVGImageUrl/26.svg?ver=1463741185',
+  'missingHubImage': 'https:test.planetradio.co.uk/assets/img/bauer-planet-radio-logo.svg',
+  'placeholderAltText': 'placeholder alt text',
+  'placeholderImage': 'http://test.planetradio.co.uk/assets/img/default_placeholder_image.gif',
+  'placeholderImageSmall': 'http:///assets/img/small_placeholder_image.png'
+},
+  'data': {
+    'primarySyndicatePosition': '',
+    'url': 'captain-america-chris-evans-bedtime-stories',
+    'title': 'Captain America Chris Evans is the next bedtime story reader on CBeebies',
+    'syndicateUrls': [
+      'magic/entertainment/television/captain-america-chris-evans-bedtime-stories/',
+      'cfm/entertainment/television/captain-america-chris-evans-bedtime-stories/',
+      'clyde/entertainment/television/captain-america-chris-evans-bedtime-stories/',
+      'clyde-2/entertainment/television/captain-america-chris-evans-bedtime-stories/'
+    ],
+    'cardImage':
+    {
+      'altText': 'Chris Evans',
+      'image': {
+        'name': '3500.jpg',
+        'fileName': '3500.jpg',
+        'mimeType': 'image/jpeg',
+        'width': 620,
+        'height': 372,
+        'fileSize': 47781,
+        'path': 'radio/media/58fe/21c7/4a62/91de/4a59/b014'
+      },
+      'caption': '',
+      'credits': 'CHRIS EVANS Â© BBC/PA'
+    },
+    'categories': [
+      {
+        '_id': '587cb5ca80222c9e7266bafa',
+        'parent': {
+          '_id': '587cb5c980222c9e7266baed',
+          'name': 'Entertainment',
+          'furl': 'entertainment'
+        },
+        'name': 'TV',
+        'furl': 'television',
+        'composed': {
+          'parent': '587cb5c980222c9e7266baed'
+        }
+      }
+    ],
+    'publications': [
+      {
+        '_id': '587cb65980222c9e7266c7c3',
+        'furl': 'magic',
+        'hostname': null,
+        'language': 'en',
+        'name': 'Magic Radio',
+        'preview': null,
+        'stationCode': 'mag',
+        'composed': {
+          'hostname': null,
+          'preview': null
+        }
+      }
+    ],
+    'composed': {
+      'categories': [
+        '587cb5ca80222c9e7266bafa',
+        '587cb5ca80222c9e7266bafa',
+        '587cb5ca80222c9e7266bafa']
+    },
+    'heroImageThumbLandscape': [
+      null
+    ],
+    'lastModifiedAt': 1493118911257,
+    'publicationDate': 1493117329000
+  }}, 'large-content-card')
