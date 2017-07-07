a81b2e782c25dd8f7cac8061a07e7c581a014051
thedapperdev <jordan.paisley@bauerxcel.co.uk>
2017-07-07 11:59:34 +0100

    Upgrade: Simplify `small_content_card/main.js` (fixes #JIG-8)

--- docs/examples/small_content_card/main.js
+++ docs/examples/small_content_card/main.js
@@ -1,16 +1,5 @@
-const filePath = '{folder}/{file}' 
-const templateLocation = {
-    folder:'small_content_card',
-    pug: 'small_content_card.pug',
-    data: 'data.json'
-}
-
-var folderPath = filePath.replace('{folder}', templateLocation.folder),
-dataPath = 'examples/'+folderPath.replace('{file}',templateLocation.data),
-pugTemplateUrl = folderPath.replace('{file}',templateLocation.pug)
-
-jQuery.getJSON(dataPath,function (json, status) {
+jQuery.getJSON('examples/small_content_card/data.json',function (json, status) {
     if (status === 'success') {
-         window.bauerSharedLibs.pug.render(pugTemplateUrl, json, '.content-card-small-test')
+         window.bauerSharedLibs.pug.render('small_content_card/small_content_card.pug', json, '.content-card-small-test')
     }
 })
