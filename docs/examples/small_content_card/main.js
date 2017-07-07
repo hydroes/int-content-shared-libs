9c6ec0b0ddf7d0844573e6d10ed1b3fb1fe73463
thedapperdev <jordan.paisley@bauerxcel.co.uk>
2017-07-07 12:11:31 +0100

    Upgrade: Change `index.html` class from `content-card-small-test` to `content-card-small` (fixes #JIG-4)

--- docs/examples/small_content_card/main.js
+++ docs/examples/small_content_card/main.js
@@ -1,5 +1,5 @@
 jQuery.getJSON('examples/small_content_card/data.json',function (json, status) {
     if (status === 'success') {
-         window.bauerSharedLibs.pug.render('small_content_card/small_content_card.pug', json, '.content-card-small-test')
+         window.bauerSharedLibs.pug.render('small_content_card/small_content_card.pug', json, '.content-card-small')
     }
 })
