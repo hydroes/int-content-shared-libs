555897b780096a9c453c4216fe0e584903d01e73
Joe Gasewicz Admin <joegasewicz@gmail.com>
2017-07-11 15:05:28 +0100

    Update: Refactor code based on Review (refs #JIG-13)

--- components/shared/js/pug/helpers/paginate.js
+++ components/shared/js/pug/helpers/paginate.js
@@ -1,25 +1,33 @@
 /**
  * @description Creates an array of objects that contain pagination data.
  * <isActive> is for css classes.
+ * Example Usage:
+ * ul.pagination
+ *  each paginate in paginates
+ *    li
+ *      a(href=paginate.href class=paginate.isActive ? 'active' : 'normal') #{paginate.display}
+ *
  * @class Paginate
- * @param {array} pagination
- * @param {number} limit The amount of pagination numbers that get displayed
+ * @param {array} pages
+ * @param {number} limit The amount of pages numbers that get displayed
  * @param {number} totalPages
  * @param {string} path eg. /bauer-radio-web
  * @param {number} currentPage The current Page that is Active
+ * @param {number} next
  */
 function Paginate (options) {
   options = options || {}
-  this.pagination = options.pagination || []
+  this.pages = options.pages || []
   this.limit = options.limit || null
   this.totalPages = options.totalPages || null
   this.path = options.path || ''
   this.currentPage = options.currentPage || null
+  this.next = options.next || null
 }
 
 /**
  * @param {number} current
- * @param {string} path - If <current> is at 1 then just returns <path> as required by Radio Web.
+ * @param {string} path - If <current> is at 1 then just return <path> as required by Radio Web.
  * @return {string}
  */
 Paginate.prototype.parsePath = function (current, path) {
@@ -56,8 +64,8 @@ Paginate.prototype.isActive = function (isThisPage) {
  * @param {string=} display (optional)
  * @return {Void}
  */
-Paginate.prototype.createSingleNumber = function (current, isActive, display) {
-  this.pagination.push({
+Paginate.prototype.createPage = function (current, isActive, display) {
+  this.pages.push({
     display: display || current,
     page: current,
     href: this.parsePath(current, this.path),
@@ -67,59 +75,52 @@ Paginate.prototype.createSingleNumber = function (current, isActive, display) {
 /**
  * @description Recursive method that builds an array based on the start
  * number being less than <limit> & checks the counter <isActive>.
- * @param {number} start
+ * @param {number} start - Either current or offset.
  * @param {number=} limit - (Optional) use either <limit>, <totalPages> or an offset value here.
  * @return {Void}
  */
-Paginate.prototype.paginateArray = function (start, limit) {
+Paginate.prototype.createPages = function (start, limit) {
   limit = limit || this.totalPages
   var _counter = start
   if (_counter <= limit) {
     var isCounterActive = this.isActive(_counter)
-    this.createSingleNumber(_counter, isCounterActive)
+    this.createPage(_counter, isCounterActive)
     _counter++
-    this.paginateArray(_counter, limit)
+    this.createPages(_counter, limit)
   }
 }
 
 module.exports = function (data) {
-  var main = {
-    limit: 10,
-    current: data.metadata.page,
-    totalPages: data.metadata.totalPages,
-    next: data.metadata.nextPage
-  }
   var paginate = new Paginate({
-    pagination: [],
-    limit: main.limit,
-    totalPages: main.totalPages,
+    pages: [],
+    limit: 10,
+    totalPages: data.metadata.totalPages,
     path: data.path,
-    currentPage: main.current
+    currentPage: data.metadata.page,
+    next: data.metadata.nextPage
   })
 
-  var prevPage = main.current
-
   function createPagination () {
-    if (main.current > 1) {
-      paginate.createSingleNumber(--prevPage, false, '<')
-      paginate.createSingleNumber(1, false, 1)
+    if (paginate.currentPage > 1) {
+      paginate.createPage(paginate.currentPage - 1, false, '<')
+      paginate.createPage(1, false, 1)
     }
 
-    var offset = paginate.counterOffset(main.current)
-    paginate.isAtLimit(main.current) ? paginate.paginateArray(offset) : paginate.paginateArray(main.current, offset)
+    var offset = paginate.counterOffset(paginate.currentPage)
+    paginate.isAtLimit(paginate.currentPage) ? paginate.createPages(offset) : paginate.createPages(paginate.currentPage, offset)
 
-    if (!paginate.isAtLimit(main.current)) {
-      paginate.createSingleNumber(paginate.totalPages, false, paginate.totalPages)
+    if (!paginate.isAtLimit(paginate.currentPage)) {
+      paginate.createPage(paginate.totalPages, false, paginate.totalPages)
     }
 
-    if (main.current < main.totalPages) {
-      paginate.createSingleNumber(main.next, false, '>')
+    if (paginate.currentPage < paginate.totalPages) {
+      paginate.createPage(paginate.next, false, '>')
     }
 
     try {
-      if (paginate.pagination.length > 0) {
-        console.log(paginate.pagination)
-        return paginate.pagination
+      if (paginate.pages.length > 0) {
+        console.log(paginate.pages)
+        return paginate.pages
       }
     } catch (err) {
       console.log('ERROR: Pagintion Array has length <= 0')
