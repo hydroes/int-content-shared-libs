d3382883b247fa5b5e450556c7bd177eccc7d645
thedapperdev <jordan.paisley@bauerxcel.co.uk>
2017-07-13 12:21:54 +0100

    Upgrade: Change `&` parent selectors in content card's SASS and refactoring (Fixes #JIG-49)

--- docs/dist/large_content_card/large_content_card.js
+++ docs/dist/large_content_card/large_content_card.js
@@ -4,13 +4,13 @@ var pug_has_own_property=Object.prototype.hasOwnProperty;
 var pug_match_html=/["&<>]/;
 function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}
 function pug_style(r){if(!r)return"";if("object"==typeof r){var t="";for(var e in r)pug_has_own_property.call(r,e)&&(t=t+e+":"+r[e]+";");return t}return r+="",";"!==r[r.length-1]?r+";":r}function large_content_card_large_content_card(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (category, date, image, images, noImage, title, url) {;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Flarge_content_card\u002Flarge_content_card.pug";
-pug_html = pug_html + "\u003Carticle class=\"content-card--large container\"\u003E";
+pug_html = pug_html + "\u003Carticle class=\"content-card content-card--large container\"\u003E";
 ;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Flarge_content_card\u002Flarge_content_card.pug";
 pug_html = pug_html + " ";
 ;pug_debug_line = 2;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Flarge_content_card\u002Flarge_content_card.pug";
 pug_html = pug_html + "\u003Cdiv class=\"row\"\u003E";
 ;pug_debug_line = 3;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Flarge_content_card\u002Flarge_content_card.pug";
-pug_html = pug_html + "\u003Ca" + (" class=\"content-card--large__link col-xs-12\""+pug_attr("href", url, true, false)) + "\u003E";
+pug_html = pug_html + "\u003Ca" + (" class=\"content-card--large__link content-card__link col-xs-12\""+pug_attr("href", url, true, false)) + "\u003E";
 ;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Flarge_card_image.pug";
 if (images) {
 ;pug_debug_line = 2;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Flarge_card_image.pug";
@@ -32,7 +32,7 @@ pug_html = pug_html + "\u003Cimg" + (pug_attr("src", noImage.lg.url, true, false
 }
 pug_html = pug_html + "\u003C\u002Fa\u003E";
 ;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Flarge_content_card\u002Flarge_content_card.pug";
-pug_html = pug_html + "\u003Cdiv class=\"content-card--large__info col-xs-12\"\u003E";
+pug_html = pug_html + "\u003Cdiv class=\"content-card--large__info content-card__info col-xs-12\"\u003E";
 ;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Ftitle.pug";
 pug_html = pug_html + "\u003Cspan class=\"title\"\u003E";
 ;pug_debug_line = 2;pug_debug_filename = "\u002FUsers\u002Fjordanpaisleyadmin\u002FDocuments\u002Fgit-repos\u002Fint-content-shared-libs\u002Fcomponents\u002Fpartials\u002Ftitle.pug";
