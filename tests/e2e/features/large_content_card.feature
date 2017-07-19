715546e49922fbc977dd13a6a550b2d3497af387
Joe Gasewicz Admin <joegasewicz@gmail.com>
2017-07-19 12:28:05 +0100

    Update: Created e2e test for the large content card (ref: JIG-7)

---
+++ tests/e2e/features/large_content_card.feature
@@ -0,0 +1,13 @@
+Feature: Large Content Card
+
+Scenario: Shows Large Card Component In Browser
+
+  Given I open the shared components page to view a large card
+  Then the small image url is "http://via.placeholder.com/350x150"
+  And the large image url is "http://via.placeholder.com/900x350"
+  And the alt text is "We asked the Love Island narrator everything you want to know about the show"
+  And the title text is "We asked the Love Island narrator everything you want to know about the show"
+  And the title link is "http://www.google.com"
+  And the category url is "http://www.bbc.co.uk"
+  And the category name is "Gossip"
+  And the date is "Gossip17 minutes ago"
---
+++ tests/e2e/features/step_definitions/large_content_card.js
@@ -0,0 +1,45 @@
+const { client } = require('nightwatch-cucumber')
+const { defineSupportCode } = require('cucumber')
+
+const imgAttr = '.large-content-card .content-card--large__link picture'
+const title = '.large-content-card .content-card--large__info span.title a'
+const category = '.large-content-card .content-card--large__info a.brand-color'
+
+defineSupportCode(({Given, Then, When}) => {
+  Given('I open the shared components page to view a large card', () => {
+    return client.url('http://localhost:3000')
+      .waitForElementVisible('#bauer-web-component-test-btn', 1000)
+  })
+
+  Then(/^the small image url is "([^"]*)"$/, (url) => {
+    return client.expect.element(imgAttr + ' source').to.have.attribute('srcset').which.contains(url)
+  })
+
+  Then(/^the large image url is "([^"]*)"$/, (url) => {
+    return client.expect.element(imgAttr + ' img').to.have.attribute('srcset').which.contains(url)
+  })
+
+  Then(/^the alt text is "([^"]*)"$/, (alt) => {
+    return client.expect.element(imgAttr + ' img').to.have.attribute('alt').which.contains(alt)
+  })
+
+  Then(/^the title text is "([^"]*)"$/, (text) => {
+    return client.expect.element(title).text.to.equal(text)
+  })
+
+  Then(/^the title link is "([^"]*)"$/, (link) => {
+    return client.expect.element(title).to.have.attribute('href').which.contains(link)
+  })
+
+  Then(/^the category url is "([^"]*)"$/, (link) => {
+    return client.expect.element(category).to.have.attribute('href').which.contains(link)
+  })
+
+  Then(/^the category name is "([^"]*)"$/, (text) => {
+    return client.expect.element(category).text.to.equal(text)
+  })
+
+  Then(/^the date is "([^"]*)"$/, (text) => {
+    return client.expect.element('.large-content-card .content-card--large__info span.hidden-xs.modified-container').text.to.equal(text)
+  })
+})
