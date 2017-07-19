e1bc855c71f437069faddf1d2647e2ccb028de8a
Ross Pickerill <ross.pickerill@bauerxcel.co.uk>
2017-07-19 11:38:07 +0100

    Fix: Wired up height watcher to the correct class names post mixin change. (refs: JIG-49)

--- docs/examples/content_cards_lg_and_nw/main.js
+++ docs/examples/content_cards_lg_and_nw/main.js
@@ -1,5 +1,22 @@
 'use strict'
+/* global jQuery */
+window.jQuery.noConflict()
+var components = {
+  'config': {
+    'callback': function (jqxhr) {
+      components.dataStatus = jqxhr.status
+      // TODO Change this to iteration... - as this will get bigger
+      if (components.dataStatus) {
+        jQuery('.large-narrow').matchHeight()
+      }
+    }
+  },
+  'dataStatus': 0
+}
 
-window.jQuery.getJSON('examples/content_cards_lg_and_nw/content_cards_lg_and_nw.json', function (data) {
-  window.bauerSharedLibs.pug.render('content_cards_lg_and_nw/content_cards_lg_and_nw.pug', data, '#content-cards-lg-nw')
+jQuery.when(jQuery.getJSON('examples/content_cards_lg_and_nw/content_cards_lg_and_nw.json')).done(function (data) {
+  var pug = window.bauerSharedLibs.pug
+  pug.render('content_cards_lg_and_nw/content_cards_lg_and_nw.pug', data, '#content-cards-lg-nw').done(function (script, textStatus, jqxhr) {
+    components.config.callback(jqxhr)
+  })
 })
