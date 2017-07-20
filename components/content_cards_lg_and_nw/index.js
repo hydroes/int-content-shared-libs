c6dd93e7e3c569707cd186713b1fb090075f017f
Brian Gouws <brian.gouws@bauermedia.co.uk>
2017-07-20 16:04:30 +0100

    Update: Refactored js so that it triggers the same functionality (refs #JIG-49)

--- components/content_cards_lg_and_nw/index.js
+++ components/content_cards_lg_and_nw/index.js
@@ -1,8 +1,5 @@
 const $ = require('jquery')
 
 module.exports = function () {
-  //  TODO: Take out `ajaxStop` wrapper on deployment, as this only there for demo site
-  $(document).ajaxStop(function () {
-    $('.large-narrow').matchHeight()
-  })
+  $('.large-narrow').matchHeight()
 }
