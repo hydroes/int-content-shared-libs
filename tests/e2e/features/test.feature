ec2b3354fdd2904b159e45aed5b5cfa9ab9c67ce
Ross Pickerill <ross.pickerill@bauerxcel.co.uk>
2017-07-20 16:49:11 +0100

    New: Added ability to run tags for the tests; phone, tablet, desktop (refs #JIG-50)

--- tests/e2e/features/test.feature
+++ tests/e2e/features/test.feature
@@ -6,10 +6,3 @@ Scenario: Clicking button
   Given I open the shared components page
   Then the test button text is "test button"
   And the buttons background color is red
-
-@desktop
-Scenario: Clicking button
-
-  Given I open the shared components page
-  Then the test button text is "test button"
-  And the buttons background color is red
--- tests/e2e/hooks.js
+++ tests/e2e/hooks.js
@@ -2,11 +2,15 @@ var {defineSupportCode} = require('cucumber')
 const {client} = require('nightwatch-cucumber')
 defineSupportCode(function ({After, Before}) {
   Before({tags: '@phone'}, function () {
-    client.resizeWindow(300, 250)
+    client.resizeWindow(375, 667)
+  })
+
+  Before('@tablet', function () {
+    client.resizeWindow(700, 940)
   })
 
   // You can use the following shorthand when only specifying tags
   Before('@desktop', function () {
-    client.resizeWindow(1100, 250)
+    client.resizeWindow(993, 940)
   })
 })
