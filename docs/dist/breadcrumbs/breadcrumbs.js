cb89e39fd97bb7699633a738ada123c7db42fc71
Ross Pickerill <ross.pickerill@bauerxcel.co.uk>
2017-08-15 12:00:39 +0100

    New: Added overriden scss to allow for the nav bar to collapse at a larger screen width and to update the container size from bootstrap (refs: #GRAZ-6)

--- docs/dist/breadcrumbs/breadcrumbs.js
+++ docs/dist/breadcrumbs/breadcrumbs.js
@@ -5,25 +5,25 @@ function pug_classes_object(r){var a="",n="";for(var o in r)o&&r[o]&&pug_has_own
 function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
 var pug_has_own_property=Object.prototype.hasOwnProperty;
 var pug_match_html=/["&<>]/;
-function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function breadcrumbs_breadcrumbs(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (data) {;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function breadcrumbs_breadcrumbs(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (data) {;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 let classNames = ""
-;pug_debug_line = 2;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 2;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 pug_html = pug_html + "\u003Col class=\"breadcrumb breadcrumbs\"\u003E";
-;pug_debug_line = 3;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 3;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 // iterate data
 ;(function(){
   var $$obj = data;
   if ('number' == typeof $$obj.length) {
       for (var index = 0, $$l = $$obj.length; index < $$l; index++) {
         var param = $$obj[index];
-;pug_debug_line = 4;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 4;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 pug_html = pug_html + "\u003Cli\u003E";
-;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 if (param.cssClass !== undefined && param.cssClass !== "" ? classNames = param.cssClass : classNames = "") {
 }
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 pug_html = pug_html + "\u003Ca" + (pug_attr("class", pug_classes([`breadcrumbs__link ${classNames}`], [true]), false, false)+pug_attr("href", param.url, true, false)) + "\u003E";
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 pug_html = pug_html + (pug_escape(null == (pug_interp = param.path) ? "" : pug_interp)) + "\u003C\u002Fa\u003E\u003C\u002Fli\u003E";
       }
   } else {
@@ -31,14 +31,14 @@ pug_html = pug_html + (pug_escape(null == (pug_interp = param.path) ? "" : pug_i
     for (var index in $$obj) {
       $$l++;
       var param = $$obj[index];
-;pug_debug_line = 4;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 4;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 pug_html = pug_html + "\u003Cli\u003E";
-;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 if (param.cssClass !== undefined && param.cssClass !== "" ? classNames = param.cssClass : classNames = "") {
 }
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 pug_html = pug_html + "\u003Ca" + (pug_attr("class", pug_classes([`breadcrumbs__link ${classNames}`], [true]), false, false)+pug_attr("href", param.url, true, false)) + "\u003E";
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Frosspickerill\u002FDocuments\u002FGit\u002Fint-content-shared-libs\u002Fcomponents\u002Fbreadcrumbs\u002Fbreadcrumbs.pug";
 pug_html = pug_html + (pug_escape(null == (pug_interp = param.path) ? "" : pug_interp)) + "\u003C\u002Fa\u003E\u003C\u002Fli\u003E";
     }
   }
