cc70a91ce8c817af764377eb6979a51fce803598
Brian Gouws <brian.gouws@bauermedia.co.uk>
2017-08-25 17:12:09 +0100

    Fix: Converted tests to use await calls (refs #GRAZ-78)

--- tests/e2e/features/step_definitions/test.js
+++ tests/e2e/features/step_definitions/test.js
@@ -2,20 +2,17 @@ const {client} = require('nightwatch-cucumber')
 const {defineSupportCode} = require('cucumber')
 
 defineSupportCode(({Given, Then, When}) => {
-  Given('I open the shared components page', (done) => {
-    client.url(client.globals.getComponentsPageUrl())
+  Given('I open the shared components page', async () => {
+    await client.url(client.globals.getComponentsPageUrl())
       .waitForElementVisible('#bauer-web-component-test-btn', 1000)
-    done()
   })
 
-  Then(/^the test button text is "([^"]*)"$/, (text, done) => {
-    client.expect.element('#bauer-web-component-test-btn').text.to.equal(text)
-    done()
+  Then(/^the test button text is "([^"]*)"$/, async (text) => {
+    await client.expect.element('#bauer-web-component-test-btn').text.to.equal(text)
   })
 
-  Then('the buttons background color is red', (done) => {
-    client.click('#bauer-web-component-test-btn')
+  Then('the buttons background color is red', async () => {
+    await client.click('#bauer-web-component-test-btn')
       .assert.cssProperty('#bauer-web-component-test-btn', 'background-color', 'rgba(255, 0, 0, 1)')
-    done()
   })
 })
