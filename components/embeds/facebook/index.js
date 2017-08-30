a43fea80663d543546ea4888cb134657f87f0e3a
Joe Gasewicz Admin <joegasewicz@gmail.com>
2017-08-30 15:49:05 +0100

    Update: Removed timeout fullbacks, replaced embeds path condition to 'embeds_'. (refs #JIG-109)

--- components/embeds/facebook/index.js
+++ components/embeds/facebook/index.js
@@ -1,29 +1,21 @@
 const $ = require('jquery')
-const faceBookWidgetInit = function () {
-  $('.embed--facebook').each(function () {
-    console.log('%cFacebook embed please', 'color: white; background: blue; padding: 1px;')
-    var _self = $(this)
-    var oembedUrl = 'https://www.facebook.com/plugins/post/oembed.json/?url=' + _self.data('embed-id')
-    $.ajax({
-      url: oembedUrl,
-      dataType: 'jsonp',
-      success: function (data) {
-        _self.html(data.html)
-      },
-      error: function (result) {
-        console.log('Facebook oembed error!')
-      }
-    })
-  })
-}
+
 module.exports = function () {
   $(document).ready(function () {
-    if ($('.embed--facebook').length) {
-      faceBookWidgetInit()
-    } else {
-      setTimeout(function () {
-        faceBookWidgetInit()
-      }, 800)
-    }
+    $('.embed--facebook').each(function () {
+      console.log('%cFacebook embed please', 'color: white; background: blue; padding: 1px;')
+      var _self = $(this)
+      var oembedUrl = 'https://www.facebook.com/plugins/post/oembed.json/?url=' + _self.data('embed-id')
+      $.ajax({
+        url: oembedUrl,
+        dataType: 'jsonp',
+        success: function (data) {
+          _self.html(data.html)
+        },
+        error: function (result) {
+          console.log('Facebook oembed error!')
+        }
+      })
+    })
   })
 }
--- components/embeds/instagram/index.js
+++ components/embeds/instagram/index.js
@@ -1,28 +1,20 @@
 const $ = require('jquery')
-const initInstagram = function () {
-  $('.embeds-instagram').each(function () {
-    var _self = $(this)
-    var oembedUrl = 'https://api.instagram.com/oembed?url=' + _self.data('embed-id')
-    $.ajax({
-      url: oembedUrl,
-      dataType: 'jsonp',
-      success: function (data) {
-        _self.html(data.html)
-      },
-      error: function (result) {
-        console.log('Instagram oembed error!')
-      }
-    })
-  })
-}
+
 module.exports = function () {
   $(document).ready(function () {
-    if ($('.embeds-instagram').length) {
-      initInstagram()
-    } else {
-      setTimeout(function () {
-        initInstagram()
-      }, 800)
-    }
+    $('.embeds-instagram').each(function () {
+      var _self = $(this)
+      var oembedUrl = 'https://api.instagram.com/oembed?url=' + _self.data('embed-id')
+      $.ajax({
+        url: oembedUrl,
+        dataType: 'jsonp',
+        success: function (data) {
+          _self.html(data.html)
+        },
+        error: function (result) {
+          console.log('Instagram oembed error!')
+        }
+      })
+    })
   })
 }
