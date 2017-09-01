bcfa1d9b02e93ac14fc529e2e52f9910f8af6cf2
Brian Gouws <brian.gouws@bauermedia.co.uk>
2017-09-01 17:40:40 +0100

    Update: Release (refs #GRAZ-53)

--- docs/dist/pagination/pagination.js
+++ docs/dist/pagination/pagination.js
@@ -5,22 +5,22 @@ function pug_classes_object(r){var a="",n="";for(var o in r)o&&r[o]&&pug_has_own
 function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
 var pug_has_own_property=Object.prototype.hasOwnProperty;
 var pug_match_html=/["&<>]/;
-function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function pagination_pagination(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (data, window) {;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function pagination_pagination(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (data, window) {;pug_debug_line = 1;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 let pages = window.bauerSharedLibs.pug.helpers.paginate(data)
-;pug_debug_line = 3;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 3;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 pug_html = pug_html + "\u003Cul class=\"pagination\"\u003E";
-;pug_debug_line = 4;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 4;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 // iterate pages
 ;(function(){
   var $$obj = pages;
   if ('number' == typeof $$obj.length) {
       for (var pug_index0 = 0, $$l = $$obj.length; pug_index0 < $$l; pug_index0++) {
         var page = $$obj[pug_index0];
-;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 pug_html = pug_html + "\u003Cli\u003E";
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 pug_html = pug_html + "\u003Ca" + (pug_attr("class", pug_classes([page.isActive ? 'pagination__link--active' : 'pagination__link'], [true]), false, false)+pug_attr("href", page.href, true, false)) + "\u003E";
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 pug_html = pug_html + (pug_escape(null == (pug_interp = page.display) ? "" : pug_interp)) + "\u003C\u002Fa\u003E\u003C\u002Fli\u003E";
       }
   } else {
@@ -28,11 +28,11 @@ pug_html = pug_html + (pug_escape(null == (pug_interp = page.display) ? "" : pug
     for (var pug_index0 in $$obj) {
       $$l++;
       var page = $$obj[pug_index0];
-;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 5;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 pug_html = pug_html + "\u003Cli\u003E";
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 pug_html = pug_html + "\u003Ca" + (pug_attr("class", pug_classes([page.isActive ? 'pagination__link--active' : 'pagination__link'], [true]), false, false)+pug_attr("href", page.href, true, false)) + "\u003E";
-;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fjoegasewiczadmin\u002FDocuments\u002Fwork\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
+;pug_debug_line = 6;pug_debug_filename = "\u002FUsers\u002Fbriangouwsadmin\u002Fworkspace\u002Fint-content-shared-libs\u002Fcomponents\u002Fpagination\u002Fpagination.pug";
 pug_html = pug_html + (pug_escape(null == (pug_interp = page.display) ? "" : pug_interp)) + "\u003C\u002Fa\u003E\u003C\u002Fli\u003E";
     }
   }
