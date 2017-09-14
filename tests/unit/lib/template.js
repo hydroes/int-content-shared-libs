24a4d52cbd9f1359bdd9632514f6a6b661811c70
Brian Gouws <brian.gouws@bauermedia.co.uk>
2017-09-14 15:11:41 +0100

    Update: Added helpers infrastructure and tests for template loader (refs #JIG-129)

---
+++ tests/unit/lib/template.js
@@ -0,0 +1,32 @@
+/* globals describe, it */
+
+const chai = require('chai')
+const assert = chai.assert
+const template = require('../../../lib/template')
+
+describe('Test template loader', function () {
+  it('Works by checking if a string is returned', function () {
+    assert.equal('string', typeof template('test'))
+  })
+  describe('Throws errors when', function () {
+    it('Template file not found', function () {
+      assert.throws(() => template('non-existent-file'), Error, 'Shared template not found: ')
+    })
+
+    it('Invalid param templateData: false', function () {
+      assert.throws(() => template('test', false), Error, 'param templateData must be an object')
+    })
+
+    it('Invalid param templateData: null', function () {
+      assert.throws(() => template('test', null), Error, 'param templateData must be an object')
+    })
+
+    it('Reserved templateData key "helper":', function () {
+      assert.throws(() => template('test', {helpers: true}), Error, 'property "helpers" is a reserved property and cannot be used')
+    })
+
+    it('Reserved templateData key "_":', function () {
+      assert.throws(() => template('test', {_: true}), Error, 'property "_" is reserved and cannot be used')
+    })
+  })
+})
