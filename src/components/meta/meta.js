edd8c92e147ef3be041d9f83e91b12deecc90235
Ross Pickerill <ross.pickerill@bauerxcel.co.uk>
2017-10-25 09:13:00 +0100

    Update: Included linting fixes (refs: #GRAZ-323)

--- src/components/meta/meta.js
+++ src/components/meta/meta.js
@@ -3,20 +3,19 @@ import PropTypes from 'prop-types'
 
 class Meta extends PureComponent {
   render () {
-    if (this.props.property){
+    if (this.props.property) {
       return (
         <meta property={this.props.property} content={this.props.content} />
       )
     }
 
-    if (this.props.name){
-      return(
+    if (this.props.name) {
+      return (
         <meta name={this.props.name} content={this.props.content} />
       )
     }
 
     return null
-    
   }
 }
 
